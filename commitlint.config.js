0db1ec98c9f5b6a0352c3a724ac50da7f484211a
Rajan Tripathi <tripathirajan3@gmail.com>
2024-12-24 20:16:07 +0530

    chore(app): setup hooks and templates # Commit Message Template
    <type>(<scope>): <subject>
    
    # Example:
    
    feat(app): add new authentication feature
    
    # Types:
    
    # - build: Changes that affect the build system or external dependencies
    
    # - chore: Other changes that don't modify src or test files
    
    # - ci: Changes to our CI configuration files and scripts
    
    # - docs: Documentation only changes
    
    # - feat: A new feature
    
    # - fix: A bug fix
    
    # - perf: A code change that improves performance
    
    # - refactor: A code change that neither fixes a bug nor adds a feature
    
    # - revert: Reverts a previous commit
    
    # - style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
    
    # - test: Adding missing tests or correcting existing tests
    
    # Scopes:
    
    # - app
    
    # - services
    
    # - lib
    
    # - config
    
    # - middlewares
    
    # - routes
    
    # - controllers
    
    # - models
    
    # - utils
    
    # - tests
    
    # - docs
    
    # Subject:
    
    # - Use sentence case
    
    # - Do not end with a period
    
    # - Limit to 72 characters or less

---
+++ commitlint.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'app',
+        'services',
+        'lib',
+        'config',
+        'middlewares',
+        'routes',
+        'controllers',
+        'models',
+        'utils',
+        'tests',
+        'docs',
+      ],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100],
+  },
+};
